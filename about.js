332d0a4cec97fd6cd1790dd9aa8ba27831bb783f
BugHunter-X <askzenai@gmail.com>
2024-12-05 19:22:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,22 @@
+```javascript
+//pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+//pages/about.js
+
+export default function About() {
+  //This will cause an error if you forget to add the return statement
+}
+```
